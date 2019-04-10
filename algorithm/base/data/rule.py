2c943ee2686a8d31f47caafe673d9497109f12cb
apple <liuxiaokai@linkingcloud.com>
2019-04-10 17:22:35 +0800

    增加接口函数

--- algorithm/base/data/rule.py
+++ algorithm/base/data/rule.py
@@ -1,2 +1,7 @@
 # coding: utf-8
 # 调度规则，将model/rule.py中的规则转换为算法需要的数据
+
+
+class Rule(object):
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -2,8 +2,22 @@
 import logging
 
 from utils.log import MyLogging
+from algorithm.truck_scheduling import TruckScheduling
+
+
+def init():
+    pass
+
+
+def compute():
+    truck_scheduling = TruckScheduling()
+    truck_scheduling.run()
+
+
+def output():
+    pass
 
 
 if __name__ == "__main__":
     MyLogging()
-    log = logging.getLogger('debug')
+    log = logging.getLogger('default')
