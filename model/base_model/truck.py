9c86c828b8e8ba2fccaed4d729da07e3729f712f
liuhao <liuhao@cloudin.cn>
2019-04-15 15:34:50 +0800

    修改bug

--- model/base_model/truck.py
+++ model/base_model/truck.py
@@ -1,7 +1,7 @@
 # coding: utf-8
-from base_.path import Path
-from base_.position import Position
-from base_.type import Truck_status
+from .base_.path import Path
+from .base_.position import Position
+from .base_.type import Truck_status
 
 from global_data import Trucks
 from model.base_model.base_.truck_inquiry_api import TruckInquiryAPI
@@ -24,21 +24,19 @@ class Truck(Path, Position, TruckInquiryAPI):
         self.city_list = []
         # 板车预计到达时间
         self.time_list = []
+        #  附近网点
+        self.near_base = []
 
-    def update(self):
-        pass
+    def update(self, base_list):
+        self.near_base = []
+        for base in base_list:
+            if self.calculate_distance(base) < 200.0:
+                self.near_base.append(base.id)
 
     def add_orders(self, order_list):
         self.orders = order_list
+        self.city_list = Path.get_best_path(self.id, order_list)
         self.status = Truck_status.TRUCK_ON_ROAD
-        self.add_paths()
-
-    def add_paths(self):
-        if not self.orders:
-            return
-
-    def calculate_cost(self, orders):
-        pass
 
     @staticmethod
     def truck_cost(car_number):
--- model/modify_model.py
+++ model/modify_model.py
@@ -2,20 +2,20 @@
 # 封装其他模块对模型的修改接口
 # 外部模块只能通过本文件的函数对模型进行修改
 
-from global_data import SUCCESS, Orders
+from global_data import SUCCESS, Orders, Trucks
 
 
 # truck接单，修改模型相关状态：truck/order/base
 # 传入参数为object
 # 返回是否修改成功
-def model_truck_take_orders(truck, orders):
+def model_truck_take_orders(truck_id, order_ids):
     result = SUCCESS
-    order_list = []
-    for index in orders:
-        order_list.append(Orders[index])
-
-    truck.add_orders(order_list=order_list)
-    for index in orders:
-        Orders.remove(Orders[index])
-
+    truck = Trucks[truck_id]
+    truck.add_orders(order_list=order_ids)
+    temp_order = []
+    for order in Orders:
+        if order.id in order_ids:
+            temp_order.append(order)
+    for order in temp_order:
+        Orders.remove(order)
     return result
