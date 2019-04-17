ea4be0116f6ff80b0e5d88c74f3b3b5ea7426f47
wangmumu <liuhaobwjc@163.com>
2019-04-17 11:33:56 +0800

    修改初始化时间

--- global_data.py
+++ global_data.py
@@ -1,4 +1,3 @@
-# coding: utf-8
 Trucks = {}
 Orders = {}
 Bases = {}
@@ -7,5 +6,5 @@ Destinations = {}
 SUCCESS = 1
 FAILED = 2
 
-# 模型开始时间 '2019-02-19 16:00:00'
-mode_start_time = 1550563200
+# model start time
+mode_start_time = '2019-02-19 16:00:00'
